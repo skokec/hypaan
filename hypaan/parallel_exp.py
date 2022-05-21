2ac1a7b530881a3ae0c3d48bd148ed33b760a616
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2022-05-21 02:01:41 +0200

    Added filename to error message when error raised during parsing of the metric file.

--- hypaan/parallel_exp.py
+++ hypaan/parallel_exp.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 import dill
@@ -24,30 +25,34 @@ def load_json_metrics(res_name):
     return metrics, {}
 
 def parse_exp(res_name, attributes_key_val, metrics, METRICS_SUFFIX, load_metrics_fn=None):
-    attrib = None
-    if os.path.exists(res_name):
-        attrib = OrderedDict()
-        for i, v in enumerate(attributes_key_val):
-            v_split = v.split("=")
-            if "=" in v:
-                attrib[v_split[0]] = "=".join(v_split[1:])
+    try:
+        attrib = None
+        if os.path.exists(res_name):
+            attrib = OrderedDict()
+            for i, v in enumerate(attributes_key_val):
+                v_split = v.split("=")
+                if "=" in v:
+                    attrib[v_split[0]] = "=".join(v_split[1:])
+                else:
+                    attrib['H%d' % i] = v
+
+            # use default json version if load_metrics_fn is not provided
+            if load_metrics_fn is None:
+                load_metrics_fn = load_json_metrics
             else:
-                attrib['H%d' % i] = v
+                load_metrics_fn = dill.loads(load_metrics_fn)
 
-        # use default json version if load_metrics_fn is not provided
-        if load_metrics_fn is None:
-            load_metrics_fn = load_json_metrics
-        else:
-            load_metrics_fn = dill.loads(load_metrics_fn)
+            # parse metrics file
+            res_metrics, res_keys = load_metrics_fn(res_name)
 
-        # parse metrics file
-        res_metrics, res_keys = load_metrics_fn(res_name)
+            # update attributes
+            if res_keys is not None and len(res_keys) > 0:
+                attrib.update(res_keys)
 
-        # update attributes
-        if res_keys is not None and len(res_keys) > 0:
-            attrib.update(res_keys)
-
-        for m in metrics:
-            if m in res_metrics:
-                attrib[METRICS_SUFFIX + m] = res_metrics[m]
+            for m in metrics:
+                if m in res_metrics:
+                    attrib[METRICS_SUFFIX + m] = res_metrics[m]
+    except Exception as e:
+        logging.error("Error while parsing file '%s': " % res_name, e)
+        raise Exception("Error while parsing file '%s': %s" % (res_name, str(e))) from e
     return attrib
